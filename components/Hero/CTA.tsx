a7a2072c10f6889a49a1c03fac0894ae4daf7c1f
OKE225 <patryk.wroblewski.2006@gmail.com>
2026-05-12 22:19:47 +0200

    Change primary button design

--- components/Hero/CTA.tsx
+++ components/Hero/CTA.tsx
@@ -12,7 +12,7 @@ const CTA = () => {
     <>
       <button
         onClick={() => handleScroll("#contact")}
-        className="bg-rose-500 text-zinc-50 font-bold text-xl max-md:text-lg max-lg:w-full px-5 py-2 mr-1 max-lg:mr-0 max-lg:mb-1 shadow-rose-950 shadow-md hover:bg-rose-600 hover:shadow-lg hover:scale-98 transition intersect:motion-preset-focus intersect:motion-delay-2000 intersect-once">
+        className="bg-rose-500 text-zinc-50 font-bold text-xl max-md:text-lg max-lg:w-full px-5 py-2 mr-1 max-lg:mr-0 max-lg:mb-1 shadow-rose-950 shadow-md hover:bg-rose-600 hover:scale-98 transition intersect:motion-preset-focus intersect:motion-delay-2000 intersect-once">
         Wyślij wiadomość
       </button>
       <button
